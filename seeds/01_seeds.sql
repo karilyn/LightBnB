74ce69e96df43f2c91dfd5beaeee43fed4ba3c06
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-02-15 14:39:16 -0800

    Modify 01_seeds to match the updated schema

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,19 +1,19 @@
-INSERT INTO users (id, name, email, password)
-VALUES (1, 'Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-(2, 'Louisa Meyer', 'jacksonrose@hotmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
-(3, 'Dominic Parks', 'victoriabobblackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password)
+VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Louisa Meyer', 'jacksonrose@hotmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Dominic Parks', 'victoriabobblackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
-INSERT INTO properties (id, owner_id, title, description, image_URL_thumbnail, image_URL_cover, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, street_address, city, province, postal_code, country, active)
-VALUES (1, 1, 'Speed Lamp', 'descripton', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', 'Canada', TRUE),
-(2, 1, 'Blank Corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85432, 6, 6, 7, '651 Nami Road', 'Bohbatev', 'Alberta', '83680', 'Canada', TRUE),
-(3, 2, 'Habit Mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, '1650 Hejto Center', 'Genwezuj', 'Newfoundland and Labrador', '44583', 'Canada', TRUE);
+INSERT INTO properties (owner_id, title, description, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code)
+VALUES (1, 'Speed Lamp', 'descripton', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 930.61, 6, 4, 8, TRUE, 'Quebec', 'Sotboske', 'Canada', '536 Namsub Highway', '28142'),
+(1, 'Blank Corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 85432, 6, 6, 7, TRUE, 'Alberta', 'Bohbatev', 'Canada', '651 Nami Road', '83680'),
+(2, 'Habit Mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 46058, 0, 5, 6, TRUE, 'Newfoundland and Labrador', 'Genwezuj', 'Canada', '1650 Hejto Center', '44583');
 
-INSERT INTO reservations (id, start_date, end_date, property_id, guest_id)
-VALUES (1, '2018-09-11', '2018-09-26', 1, 1),
-(2, '2019-01-04', '2019-02-01', 2, 2),
-(3, '2021-10-01', '2021-10-14', 3, 3);
+INSERT INTO reservations (property_id, guest_id, start_date, end_date)
+VALUES (1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, '2019-01-04', '2019-02-01'),
+(3, 3, '2021-10-01', '2021-10-14');
 
-INSERT INTO property_reviews (id, guest_id, property_id, reservation_id, rating, message)
-VALUES (1, 3, 2, 1, 3, 'messages'),
-(2, 2, 2, 2, 4, 'messages'),
-(3, 3, 1, 3, 4, 'messages');
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (2, 2, 1, 3, 'messages'),
+(6, 2, 2, 4, 'messages'),
+(1, 1, 3, 4, 'messages');
