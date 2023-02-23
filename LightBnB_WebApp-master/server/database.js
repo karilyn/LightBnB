eb1f19045bc0fb903409f94df2cf9e784436f6f9
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-02-23 15:36:18 -0800

    Require database adapter file rather than including the pool directly

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,13 +1,4 @@
-const { Pool } = require('pg');
-require("dotenv").config();
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-});
+const pool = require('./db/db');
 
 // Users
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # LightBnB
+A simple, multi-page AirBnB clone that uses server-side JavaScript to display the information from queries to web pages via SQL queries. 
