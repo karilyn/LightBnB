984574f5eb3624354db4b9e2be5818ee9b6ed1c7
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-02-23 15:37:06 -0800

    Add a database file as a single place to configure the database

---
+++ LightBnB_WebApp-master/server/db/db.js
@@ -0,0 +1,18 @@
+// All interactions with the server run through this file
+const { Pool } = require('pg');
+
+require("dotenv").config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback)
+  },
+}
