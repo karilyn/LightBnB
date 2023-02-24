97644104403a9e7fc99962cc22e2ec22d89de6a4
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-02-23 20:32:59 -0800

    Add a catch to getAllListings AJAX request

--- LightBnB_WebApp-master/public/javascript/index.js
+++ LightBnB_WebApp-master/public/javascript/index.js
@@ -1,10 +1,12 @@
 $(() => {
-  getAllListings().then(function( json ) {
+  getAllListings()
+    .then(function( json ) {
     propertyListings.addProperties(json.properties);
-    views_manager.show('listings');
+    views_manager.show('listings')
+    .catch((err) => {
+      console.error(err);
+      throw err;
+    })
   });
 });
 
-//* ^^^ there needs to be a catch up here because if it fails it
-//* will be an uncaught promise error
-//* we might want to just turn the variable in addProperties into properties instead of json.properties
