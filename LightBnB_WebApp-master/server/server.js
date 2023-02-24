bf872263e7bcb47d9b7fce2e2b06f8fb6b37998a
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-02-23 20:30:26 -0800

    Remove unnecessary comments

--- LightBnB_WebApp-master/server/server.js
+++ LightBnB_WebApp-master/server/server.js
@@ -8,7 +8,6 @@ const express = require('express');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 
-/// this is an instance of an express server
 const app = express();
 
 app.use(cookieSession({
@@ -28,16 +27,11 @@ app.use('/api', apiRouter);
 // /user/endpoints
 const userRouter = express.Router();
 userRoutes(userRouter, database);
-/// any of the user paths are now scoped into this sub router so that it joins the main list at the end.
-/// this is basically a helper function for our routers
+
 app.use('/users', userRouter);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-
-/// we're registering this handler into the main app, so after we call it, these handlers and paths are stored in the app object
-/// we think this is waiting, but it happens immediately and then listens later
-/// imagine it as a list of all our route handlers, BUT we can also break it up into sub paths
 app.get("/test", (req, res) => {
   res.send("🤗");
 });
